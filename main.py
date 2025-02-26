34ebb24731d4a6babd9441ec0ae72a890480885a
Vikram-Suwasiya <vikram@xlpxl.com>
2025-02-26 16:50:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ def search(query: dict):
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
